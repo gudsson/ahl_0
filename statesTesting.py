9b155e324bf5769e6db5e7ae1a7d1a756b045943
Jay Gudsson <gudsson@gmail.com>
2020-07-05 20:38:26 -0700

    successfully clear expired penalties

--- statesTesting.py
+++ statesTesting.py
@@ -19,54 +19,59 @@ class GameStates(object):
     def active_penalties(self):
         return self._active_penalties
 
-    # @active_penalties.setter
-    # def active_penalties(self, value):
-    #     print(value)
-    #         # value['expires_at'] = '69'
-    #         # self.active_penalties = self.active_penalties + [value]
-    
-    # def append(self, value):
-    #     self.active_penalties = self.active_penalties + [value]
-    #     # return self._active_penalties
-    #     # value['expires_at'] = dt.strptime(value['taken_at'], C.FMT) + dt.strptime('2:00', C.FMT)
-    #     # self.determine_expiry(value)
-    #     # self.active_penalties = self.active_penalties + [value.update({"hello": "test"})]
-
-    def determine_expiry(self, value):
-        # self.expires_at = dt.strptime(value['taken_at'], C.FMT) + dt.strptime('2:00', C.FMT)
-        # return self.expires_at
-        print("success")
-
     def add_penalty(self, value):
         print(value)
-        print(f"active {value['team']} penalties: {len(self._active_penalties[value['team']])}")
+        print(f"active {value['team']} penalties: {len(self._active_penalties[value['team']]) + 1}")
         if len(self._active_penalties[value['team']]) < 2:
             self._manpower[value['team']] -= 1
-            value['expires_at'] = (datetime.strptime(value['taken_at'], C.FMT) + timedelta(minutes=2)).strftime(C.FMT) #'dt.strptime('2:00', C.FMT)
-            self._active_penalties[value['team']].append(value)
+            value['expires_at'] = datetime.strptime(value['taken_at'], C.FMT) + timedelta(minutes=value['pim']) #'dt.strptime('2:00', C.FMT)
+
         else:
-            print(f'need to queue penalties')
-        # value['player_number'] = value['player_number'] + 1
-        # self.active_penalties = self.active_penalties + [value]
+            self.penalty_number = len(self._active_penalties[value['team']])
+            self.penalty_to_follow = self.penalty_number - 2
+            value['expires_at'] = datetime.strptime(self._active_penalties[value['team']][self.penalty_to_follow]['expires_at'], C.FMT) + timedelta(minutes=value['pim'])
+
+        if value['expires_at'] > datetime.strptime('20:00', C.FMT): # if penalty expires after the end of the period, move expiry to beginning of next period.
+            value['expires_at'] = (value['expires_at'] - timedelta(minutes=20)).strftime(C.FMT)
+            value['period_expires'] = value['period_taken'] + 1 #pass
+        else:
+            value['expires_at'] = (value['expires_at']).strftime(C.FMT) #convert time to string
+            value['period_expires'] = value['period_taken'] #else stay in current period
+        self._active_penalties[value['team']].append(value)
+
+    def clear_expired(self, value):
+        for team in self._active_penalties:
+            
+            for penalty in self._active_penalties[team][:]:
+               
+                if (penalty['period_expires'] < value['period']) or ((penalty['period_expires'] == value['period']) and (penalty['expires_at'] <= value['time'])):
+                    
+                    if len(self._active_penalties[team]) <= 2:
+                        self._manpower[team] += 1
+
+                    self._active_penalties[team].remove(penalty)
+                    print(self._manpower)
+                    
+
+    def event_check(self, value):
+        self.clear_expired(value)
+        # if value['event'] == 'GOAL':
+        #     print("hello")
 
 
 if __name__ == "__main__":
     states = GameStates()
+    states.add_penalty({ 'team': 'home', 'player_number': 27, 'pim': 2, 'taken_at': '16:00', 'period_taken': 1 })
+    states.add_penalty({ 'team': 'home', 'player_number': 28, 'pim': 2, 'taken_at': '16:30', 'period_taken': 1 })
+    states.add_penalty({ 'team': 'home', 'player_number': 29, 'pim': 2, 'taken_at': '17:00', 'period_taken': 1 })
+    states.add_penalty({ 'team': 'home', 'player_number': 30, 'pim': 2, 'taken_at': '17:30', 'period_taken': 1 })
     # print(states.manpower)
-    # states.manpower["home"] -= 1
-    # print(states.manpower)
-    # states.active_penalties = { 'player_number': 27, 'taken_at': '1:00' }
-    # states.active_penalties["home"].append({ 'player_number': 27, 'taken_at': '1:00' })
-    states.add_penalty({ 'team': 'home', 'player_number': 27, 'taken_at': '1:00' })
-    states.add_penalty({ 'team': 'home', 'player_number': 27, 'taken_at': '1:30' })
-    states.add_penalty({ 'team': 'home', 'player_number': 27, 'taken_at': '2:00' })
-    states.add_penalty({ 'team': 'home', 'player_number': 27, 'taken_at': '2:30' })
-    print(states.manpower)
-    print(states.active_penalties)
+    # for team in states.active_penalties:
+    #     for penalty in states.active_penalties[team]:
+    #         print(penalty)
 
-    # time1 = '1:00'
-    # time2 = '1:30'
+    states.event_check({ 'event': 'GOAL', 'team': 'away', 'time': '00:15', 'period': 2})
 
-    # tdelta = dt.strptime(time2, C.FMT) - dt.strptime(time1, C.FMT)
-
-    # print(tdelta)
+    for team in states.active_penalties:
+        for penalty in states.active_penalties[team]:
+            print(penalty)
