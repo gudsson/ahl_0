9cd64510f70189bfcdc27b82250b2aea65593195
Jay Gudsson <gudsson@gmail.com>
2020-10-12 20:58:45 -0700

    revert

--- gameClasses.py
+++ gameClasses.py
@@ -36,7 +36,11 @@ class Game2(object):
 
 class Game(object):
 <<<<<<< HEAD
+<<<<<<< HEAD
     def __init__(self, game_id=0, data_queried = []): #manpower = {"home": 5, "away": 5, "homeGoalie": True, "awayGoalie": True}):
+=======
+    def __init__(self, game_id=0, data_queried = [], manpower = {"home": 5, "away": 5}):#, data=None):
+>>>>>>> parent of 99ecd43... add pulled goalie columns to db
 =======
     def __init__(self, game_id=0, data_queried = [], manpower = {"home": 5, "away": 5}):#, data=None):
 >>>>>>> parent of 99ecd43... add pulled goalie columns to db
